48b1fc8e8dd2599df88d1477fdaecf1b44f90e22
Noussayr DERBEL <Thinkpad@192.168.1.25>
2021-06-27 23:33:08 +0100

    [services] : offreServices

--- Project/src/tn/connect/offer/services/OfferService.java
+++ Project/src/tn/connect/offer/services/OfferService.java
@@ -1,5 +1,36 @@
 package tn.connect.offer.services;
 
 public class OfferService {
+    private Connection cnx;
+    private Statement st;
+    private PreparedStatement pre;
 
+    public ServiceStage() {
+        cnx DBConnection.getInstance().getCnx();
+        
+    }
+    public void addStage(Stages s)throws SQLException{
+        String req = "INSERT INTO STAGES (company,"
+                + "startDate"
+                + ",endDate"
+                + ",theme,"
+                + "title,"
+                + "description"
+                + ",candidat)values ('"
+                +s.getCompany()+"','"
+                +s.getStartDate()+"','"
+                +s.getEndDate()+"','"
+                +s.getTheme()+"','"
+                +s.getTitle()+"','"
+                +s.getDescription()+"','"+s.getCandidat()+"');";  
+            Statement ste = cnx.createStatement();
+         ste.executeUpdate(req);
+         System.out.println("Valeurs Insérré");
+        }
+    public List<Stages> displayStages(){
+        String req= "SELECT * FROM STAGES";
+        return Stages;
+        
+    }
+	
 }
