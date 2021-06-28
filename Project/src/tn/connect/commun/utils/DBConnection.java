2590e39d69707dd20b004b55d3dab563a8c4d984
Thinkpad <Thinkpad@192.168.1.25>
2021-06-28 17:19:22 +0100

    [commun] : updated DB Connection

--- Project/src/tn/connect/commun/utils/DBConnection.java
+++ Project/src/tn/connect/commun/utils/DBConnection.java
@@ -1,7 +1,8 @@
 package tn.connect.commun.utils;
 
-
 import java.sql.DriverManager;
+import java.sql.Connection;
+
 import java.sql.SQLException;
 
 /*
@@ -9,38 +10,37 @@ import java.sql.SQLException;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Thinkpad
  */
-
 public class DBConnection {
+
     public static DBConnection instance;
-    
-    private String URL="jdbc:mysql://127.0.0.1:3306/connect";
-    private String LOGIN="root";
-    private String PASSWORD="";
-    private DBConnection cnx;
-    
-    private DBConnection(){
-        try{
-            cnx = (DBConnection) DriverManager.getConnection(URL,LOGIN,PASSWORD);
+
+    private String URL = "jdbc:mysql://127.0.0.1:3306/connect";
+    private String LOGIN = "root";
+    private String PASSWORD = "";
+    private Connection cnx;
+
+    private DBConnection() {
+        try {
+            cnx = DriverManager.getConnection(URL, LOGIN, PASSWORD);
             System.out.println("Connection Established");
-        }catch(SQLException ex){
+        } catch (SQLException ex) {
             System.err.println(ex);
         }
-  
+
     }
-    
-    public static DBConnection getInstance(){
-        if(instance == null){
+
+    public static DBConnection getInstance() {
+        if (instance == null) {
             instance = new DBConnection();
         }
         return instance;
     }
-    public DBConnection getCnx(){
+
+    public Connection getCnx() {
         return cnx;
     }
 }
-
