44e35957eb46af4e157b6d81af5b1e6f1bffebf6
lkhleya <86523827+lkhlEya@users.noreply.github.com>
2021-07-13 19:27:29 +0100

    [Commun] : HomePage

--- Project/src/tn/connect/gui/commun/HomePage/HomePageControler.java
+++ Project/src/tn/connect/gui/commun/HomePage/HomePageControler.java
@@ -1,5 +1,39 @@
 package tn.connect.gui.commun.HomePage;
 
-public class HomePageControler {
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class HomePageControler extends Application{
+	
+	 @Override
+	    public void start(Stage primaryStage) {
+	        
+	        try {
+	            Parent root = FXMLLoader.load(getClass().getResource("HomePageUI.fxml"));
+	            
+	            Scene scene = new Scene(root);
+	            
+	            primaryStage.setTitle("HomePageUI!");
+	            primaryStage.setScene(scene);
+	            primaryStage.show();
+	            
+	        } catch (IOException ex) {
+	            
+	            System.out.println(ex.getMessage());
+	        
+	        }
+	    }
+
+	    /**
+	     * @param args the command line arguments
+	     */
+	    public static void main(String[] args) {
+	        launch(args);
+	    }
+	    
 
 }
